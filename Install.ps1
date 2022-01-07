91070e582ab47d572bca7544a9bf12518ecc7b9e
Jeremy Hodges <jhodges@horizontal.com>
2022-01-07 14:53:20 -0600

    Added installation scipt to root folder

--- Install.ps1
+++ Install.ps1
@@ -1,30 +1,7 @@
-function Update-MyModules {
+#Run this script from your machine to install the Module for the first time
 
-$AsAdministrator = [Security.Principal.WindowsIdentity]::GetCurrent().Groups -contains 'S-1-5-32-544'
-
-if ($AsAdministrator -ne "True") {
-    Throw "Please run powershell as administrator"
-}
-
-#Check for git
-try {
-    git --version
-    if ($? -eq $false) {
-        }
-    }
-    catch {
-        Throw "Terminating, command requires git, please install git"
-    }
-
-#Check for Modules
-If (Test-Path "$Env:ProgramFiles\WindowsPowerShell\Modules\My") {
-
-} else 
-{
-    Throw "Module doesn't exist in path $Env:ProgramFiles\WindowsPowerShell\Modules\, either module is not installed or module is installed in a path not supported by this command."
-}
-
-#Check for Previous update files and delete them
+#Removing old installation folder in case this was tried once before and failed and didn't clean up files
+#Or in case you happen to have a folder named "$Env:SystemDrive\temp\MyModule\My"
 If (Test-Path "$Env:SystemDrive\temp\MyModule\My") {
     Write-Warning "Update folder exists, creating prompt JUST IN CASE you need the old update folder in the temp folder, however you probably don't"
     $confirmation = Read-Host "Update folder $Env:SystemDrive\temp\MyModule\My exists, to delete folder and download latest update folder type 'yes'"
@@ -36,19 +13,20 @@ If (Test-Path "$Env:SystemDrive\temp\MyModule\My") {
         }
 }
 
-#Install
 Write-Host "Creating Directory"
 New-Item -Path "$Env:SystemDrive\temp\MyModule" -Name "My" -ItemType "directory"
+
 Write-Host "Downloading and unzipping files"
 Invoke-WebRequest -Uri "https://github.com/remeric/MyScripts/archive/refs/heads/main.zip" -OutFile "$Env:SystemDrive\temp\MyModule\My\My.zip"
 Expand-Archive -Path "$Env:SystemDrive\temp\MyModule\My\My.zip" -DestinationPath "$Env:SystemDrive\temp\MyModule\My"
+
+#Just in case you have installed it before and have an old version
 Write-Host "Deleting old module version"
 Remove-Item "$Env:ProgramFiles\WindowsPowerShell\Modules\My" -Recurse 
 Remove-Item "$Env:ProgramFiles\WindowsPowerShell\Modules\My.Logins" -Recurse
+
 Write-Host "Installing (copying) new module version"
 Copy-item -path "$Env:SystemDrive\temp\MyModule\My\MyScripts-main\Modules\*" "$Env:ProgramFiles\WindowsPowerShell\Modules" -Recurse
+
 Write-Host "Cleaning Up Files"
 Remove-item $Env:SystemDrive\temp\MyModule\My -Recurse
-
-}
-
--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 Collection of scripts inside a module that I use - organized and create specifically for my work and my style - but you are welcome to them as well.  Since I anticipate to be the only one using this scripts may or may not be pretty, efficient, or possibly even make sense for you.  If it gets popular I will consider making everything nice, neat, efficient, and per the book.
 
 # Installation
-
+1. Run powershell as Administrator, then run the following script
+2. Run Install.ps1 from your machine (you can just copy the code or you can download the entire git repository, doesn't matter it downloads files in the script)
 
 # Warning
 WARNING: USE AT YOUR OWN RISK.  BE SURE TO REVIEW ALL CODE AND TEST BEFORE IMPLEMENTING.  YOU ARE RESPONSIBLE FOR ANY IMPLEMENTATION.
