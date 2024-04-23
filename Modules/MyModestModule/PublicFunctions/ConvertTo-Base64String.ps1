9a398516bd64d21329f3b0ed82c3aad90989283f
Jeremy Hodges <jshodges@hotmail.com>
2024-04-22 20:31:58 -0500

    removed white space flagged on code analysis

--- Modules/MyModestModule/PublicFunctions/ConvertTo-Base64String.ps1
+++ Modules/MyModestModule/PublicFunctions/ConvertTo-Base64String.ps1
@@ -6,19 +6,19 @@
 
 function ConvertTo-MyBase64String {
 
-    Param (         
-      [Parameter(Mandatory)]         
-      [ValidateScript({                 
-        if (-Not ($_ | Test-Path) ) {                     
-          throw "The file or folder $_ does not exist"                 
-        }                
-        if (-Not ($_ | Test-Path -PathType Leaf) ) {                     
-          throw "The Path argument must be a file. Folder paths are not allowed."                 
-        }                
-        return $true             
-      })]        
-      [string] $Path     
-    )  
+    Param (
+      [Parameter(Mandatory)]
+      [ValidateScript({
+        if (-Not ($_ | Test-Path) ) {
+          throw "The file or folder $_ does not exist"
+        }
+        if (-Not ($_ | Test-Path -PathType Leaf) ) {
+          throw "The Path argument must be a file. Folder paths are not allowed."
+        }
+        return $true
+      })]
+      [string] $Path
+    )
 
     $fileContentBytes = get-content $Path -AsByteStream
     $Base64Cert = [System.Convert]::ToBase64String($fileContentBytes)
